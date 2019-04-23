082edbaf00ef2b0d900cf60af7f8b5ac6826f3be
dixitomkar1809 <dixitomkar1809@gmail.com>
2019-04-22 21:45:47 -0500

    Added parking lot notes

---
+++ CtCi/Object Oriented Design/parkingLot.py
@@ -0,0 +1,32 @@
+# Author: Omkar Dixit
+# Email: ond170030@utdallas.edu
+
+# Parking Lot
+
+"""
+Parking lot will have 
+    - Vehicles
+        - Type of vehicle 
+            - Bike 
+            - Car
+        - License number
+        - HandicapFlag
+
+    - Spaces
+        -Type of Space
+            - Bike
+            - Compact
+            - Large
+        - Level
+        - SpotNumber (starts from 0 on each level)
+        - HandicapFlag
+
+    - ParkingLot
+        - Map with key being level and value beign spaces on that level
+        - addSpaceByLevel(level)
+        - getSpace(Vehicle)
+        - getSpaceByLevel(Vehicle)
+        - getHandicapSpaces(Vehicle)
+        - 
+
+"""
