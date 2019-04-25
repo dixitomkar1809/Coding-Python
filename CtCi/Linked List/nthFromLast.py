e63f13806468053d879552482f9f941b8262a025
dixitomkar1809 <dixitomkar1809@gmail.com>
2019-04-25 15:32:46 -0500

    Adding nth from last note

---
+++ CtCi/Linked List/nthFromLast.py
@@ -0,0 +1,40 @@
+# Author: Omkar Dixit
+# Email: ond170030@utdallas.edu
+
+# Nth from last
+
+import sys
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def nthFromLast(head, n):
+    fast = slow = head
+    for i in range(n+1):
+        if not fast:
+            return "Out of Bounds"
+        fast = fast.next
+    while fast:
+        slow = slow.next
+        fast = fast.next
+    return slow.val
+
+if __name__=="__main__":
+    if len(sys.argv) < 3:
+        print("List not detected")
+    else:
+        n = int(sys.argv[1])
+        head = Node(sys.argv[2])
+        curr = head
+        for i in sys.argv[3:]:
+            node = Node(i)
+            curr.next = node
+            curr = node
+        curr = head
+        while curr:
+            print(curr.val, end=" ")
+            curr = curr.next
+        print()
+        print(nthFromLast(head, n))
