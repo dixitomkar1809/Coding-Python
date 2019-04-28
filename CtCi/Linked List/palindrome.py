cc4c334d37c54f82ff590a5c6d5ac6c0017b51cc
dixitomkar1809 <dixitomkar1809@gmail.com>
2019-04-28 00:12:27 -0500

    Adding

---
+++ CtCi/Linked List/palindrome.py
@@ -0,0 +1,40 @@
+# Author: Omkar Dixit
+# Email: ond170030@utdallas.edu
+
+# Palindrome
+
+import sys
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def palindrome(head):
+    fast = slow = head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+    prev = next = None
+    curr = slow
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    while prev:
+        if head.val != prev.val:
+            return False
+        head = head.next
+        prev = prev.next
+    return True
+
+if __name__=="__main__":
+    # head = Node(1)
+    # head.next = Node(2)
+    # head.next.next = Node(2)
+    # head.next.next.next = Node(1)
+    head = Node(1)
+    head.next = Node(3)
+    head.next.next = Node(4)
+    print(palindrome(head))
