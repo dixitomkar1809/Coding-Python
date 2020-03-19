9ff624076c5afba38b181f441a1aeab07fe1bcb2
dixitomkar1809 <omedxt@gmail.com>
2020-03-18 21:22:53 -0500

    Updates min stack

--- CtCi/Stack&Queue/minStack.py
+++ CtCi/Stack&Queue/minStack.py
@@ -9,37 +9,54 @@ which returns the minimum element? Push, pop and min should all operate in 0(1) 
 import sys
 
 class minStack:
-    def __init__(self):
-        self.stack = []
-        self.minStack = []
+    def __init__(self, capacity):
+        self.stack = [float('-inf')] * capacity
+        self.minStack = [float('-inf')] * capacity
+        self.size = 0
+        self.capacity = capacity
 
-    def push(self, x):
-        self.stack.append(x)
-        if not self.minStack or self.minStack[-1] >= x:
-            self.minStack.append(x)
-        return
+    def push(self, val):
+        if (self.isFull()):
+            return False
+        if self.size == 0:
+            self.stack[self.size] = val
+            self.minStack[self.size] = val
+            self.size += 1
+        else:
+            self.stack[self.size] = val
+            if self.minStack[self.size - 1] > self.stack[self.size]:
+                self.minStack[self.size] = self.stack[self.size]
+            else:
+                self.minStack[self.size] =  self.minStack[self.size - 1]
+            self.size += 1
+        return True
     
     def pop(self):
-        if not self.stack:
+        if self.isEmpty():
             return None
-        x = self.stack.pop()
-        if x == self.minStack[-1]:
-            self.minStack.pop()
-        return x
-    
+        val = self.stack[self.size-1]
+        self.size -= 1
+        return val
+
     def getMin(self):
-        if not self.minStack:
+        if self.isEmpty():
             return None
-        return self.minStack[-1]
+        return self.minStack[self.size-1]
 
     def top(self):
-        if not self.stack:
+        if self.isEmpty():
             return None
-        return self.stack[-1]
+        return self.stack[self.size-1]
+
+    def isEmpty(self):
+        return self.size == 0
+    
+    def isFull(self):
+        return self.size == self.capacity
 
 
 if __name__=="__main__":
-    ms = minStack()
+    ms = minStack(5)
     ms.push(-2)
     ms.push(0)
     ms.push(-3)
