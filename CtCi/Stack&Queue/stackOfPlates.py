9fa261280e70adeaf8fad4bf542a09596f9e1e87
dixitomkar1809 <omedxt@gmail.com>
2020-04-01 22:54:48 -0500

    Adds the stack of plates, need to work on the follow up question

--- CtCi/Stack&Queue/stackOfPlates.py
+++ CtCi/Stack&Queue/stackOfPlates.py
@@ -11,3 +11,82 @@ SetOfStacks. push() and SetOfStacks. pop() should behave identically to a single
 FOLLOW UP
 Implement a function popAt ( int index) which performs a pop operation on a specific sub-stack.
 '''
+
+class Stack:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.stack = [float('-inf')] * self.capacity
+        self.size = 0
+
+    def pop(self):
+        if self.isEmpty():
+            return None
+        val = self.stack[self.size-1]
+        self.size-=1
+        return val
+
+    def push(self, value):
+        if self.isFull():
+            return False
+        self.stack[self.size] = value
+        self.size += 1
+        return True
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def isFull(self):
+        return self.size == self.capacity
+
+class setOfStacks:
+    def  __init__(self, capacity): 
+        self.setOfStacks = []
+        self.stackCapacity = capacity
+        self.noOfStacks = 0
+        self.size = 0
+        self.addStack(Stack(self.stackCapacity))
+    
+    def addStack(self, newStack):
+        self.setOfStacks.append(newStack)
+        self.noOfStacks += 1
+
+    def getLastStack(self):
+        return self.setOfStacks[-1]
+
+    def pop(self):
+        last = self.getLastStack()
+        if not last: return None
+        value = last.pop()
+        if last.isEmpty():
+            del self.setOfStacks[-1]
+            self.noOfStacks -= 1
+        return value
+    
+    # def pop(self, i):
+    #     pass
+
+    def push(self, value):
+        last = self.getLastStack()
+        if not last.isFull():
+            last.push(value)
+            return True
+        print('here')
+        newStack = Stack(self.stackCapacity)
+        newStack.push(value)
+        self.addStack(newStack)
+        return True
+
+if __name__ == '__main__':
+    stackOfPlates = setOfStacks(6)
+    stackOfPlates.push(1)
+    stackOfPlates.push(1)
+    stackOfPlates.push(1)
+    stackOfPlates.push(1)
+    stackOfPlates.push(1)
+    stackOfPlates.push(1)
+    stackOfPlates.push(2)
+    stackOfPlates.pop()
+    for stack in stackOfPlates.setOfStacks:
+        print(stack.stack)
+
+
