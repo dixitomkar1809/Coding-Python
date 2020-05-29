4aecca32c392b76f5bf39a32b1f0ce18b556f45f
dixitomkar1809 <omedxt@gmail.com>
2020-05-29 00:00:37 -0500

    Adds k Reverse Linked List

---
+++ GFG/random/kReverseLinkedList.py
@@ -0,0 +1,55 @@
+# Author: Omkar Dixit
+# Email: omedxt@gmail.com   
+
+'''
+Given a linked list, write a function to reverse every k nodes (where k is an input to the function).
+'''
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Solution:
+    def reverse(self, root):
+        next = prev = None
+        curr = root
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
+
+    def kReverse(self, root, k):
+        next = prev = None
+        curr = root
+        count = k
+        while curr and count != 0:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next 
+            count -= 1
+        if next:
+            root.next = self.kReverse(next, k)
+        return prev
+
+    def printList(self, root):
+        curr = root
+        while curr:
+            print(curr.value, end=" ")
+            curr = curr.next
+        print()
+
+if __name__=="__main__":
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    head.next.next.next.next.next.next = Node(7)
+    head.next.next.next.next.next.next.next = Node(8)
+    sol = Solution()
+    newHead = sol.kReverse(head, 5)
+    sol.printList(newHead)
