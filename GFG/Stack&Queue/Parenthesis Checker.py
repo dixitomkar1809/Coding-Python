6322a054ef7172412ba47eff0097c90c4d74aaf9
dixitomkar1809 <omedxt@gmail.com>
2020-05-31 14:20:18 -0500

    Adds parenthesis checker

--- GFG/Stack&Queue/Parenthesis Checker.py
+++ GFG/Stack&Queue/Parenthesis Checker.py
@@ -27,3 +27,50 @@ balanced
 balanced
 not balanced
 '''
+
+class Solution:
+    def parenthesisChecker(self, parenthesis):
+        stack = []
+        for char in parenthesis:
+            if self.isOpening(char):
+                stack.append(char)
+            else:
+                if not (self.isMatching(stack.pop(), char)):
+                    return False
+        if stack:
+            return False
+        return True
+    
+    def isOpening(self, char):
+        if char == '(':
+            return True
+        elif char == '[':
+            return True
+        elif char == '{':
+            return True
+        else:
+            return False
+
+    def isClosing(self, char):
+        if char == ')':
+            return True
+        elif char == ']':
+            return True
+        elif char == '}':
+            return True
+        else:
+            return False
+
+    def isMatching(self, opening, closing):
+        if closing == ')':
+            return opening == '('
+        elif closing == ']':
+            return opening == '['
+        elif closing == '}':
+            return opening == '{'
+        return False
+
+if __name__=="__main__":
+    str = "[({)]"
+    sol = Solution()
+    print(sol.parenthesisChecker(list(str)))
