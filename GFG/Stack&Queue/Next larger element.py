c8143b472d9096c9dd379a923b5940db9aa5586f
dixitomkar1809 <omedxt@gmail.com>
2020-05-31 15:34:05 -0500

    Adds next larger element

--- GFG/Stack&Queue/Next larger element.py
+++ GFG/Stack&Queue/Next larger element.py
@@ -3,25 +3,33 @@
 
 '''
 Given an array A of size N having distinct elements, the task is to find the next greater element for each element of the array in order of their appearance in the array. If no such element exists, output -1 
-
-Input:
-The first line of input contains a single integer T denoting the number of test cases.Then T test cases follow. Each test case consists of two lines. The first line contains an integer N denoting the size of the array. The Second line of each test case contains N space separated positive integers denoting the values/elements in the array A.
-
-Output:
-For each test case, print in a new line, the next greater element for each array element separated by space in order.
-
-Constraints:
-1 <= T <= 100
-1 <= N <= 107
-1 <= Ai <= 1018
-Example:
-Input
-2
-4
-1 3 2 4
-4
-4 3 2 1
-Output
-3 4 4 -1
--1 -1 -1 -1
 '''
+
+# Time Complexity: O(n)
+
+class Solution():
+    def nextLargerElement(self, arr):
+        stack = [0]
+        s = [-1] * len(arr)
+        for i in range(1, len(arr)):
+            next = arr[i]
+            if stack:
+                j = stack.pop();
+                element = arr[j]
+                while next > element:
+                    s[j] = next
+                    if not stack: break
+                    j = stack.pop()
+                    element = arr[j]
+                if next < element:
+                    stack.append(j)
+            stack.append(i)
+        while stack:
+            s[stack.pop()] = -1
+        return s
+
+if __name__=="__main__":
+    arr = [1, 3, 2, 4]
+    arr2 = [4, 3, 2, 1]
+    sol = Solution()
+    print(sol.nextLargerElement(arr2))
