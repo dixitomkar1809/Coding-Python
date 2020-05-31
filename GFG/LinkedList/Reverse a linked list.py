c44e919edef11763f67f3d4045edee3877fc6059
dixitomkar1809 <omedxt@gmail.com>
2020-05-31 13:49:45 -0500

    Adds Reverse Linked List

--- GFG/LinkedList/Reverse a linked list.py
+++ GFG/LinkedList/Reverse a linked list.py
@@ -44,3 +44,39 @@ Output:
 6 5 4 3 2 1
 10 9 8 7 2
 '''
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Solution:
+    def reverse(self, root):
+        next = prev = None
+        curr = root
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
+
+    def printList(self, root):
+        curr = root
+        while curr:
+            print(curr.value, end=" ")
+            curr = curr.next
+        print()
+
+if __name__=="__main__":
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    head.next.next.next.next.next.next = Node(7)
+    head.next.next.next.next.next.next.next = Node(8)
+    sol = Solution()
+    newHead = sol.reverse(head)
+    sol.printList(newHead)
