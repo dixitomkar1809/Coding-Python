774e876937a9670e1791feb3102153028ecf1e23
dixitomkar1809 <omedxt@gmail.com>
2020-06-16 22:19:23 -0500

    Adds minimize sum of product

---
+++ GFG/Greedy/minimizeSumOfProduct.py
@@ -0,0 +1,20 @@
+# Author: Omkar Dixit
+# Email: omedxt@gmail.com
+
+'''
+You are given two arrays, A and B, of equal size N. The task is to find the minimum value of A[0] * B[0] + A[1] * B[1] +…+ A[N-1] * B[N-1], where shuffling of elements of arrays A and B is allowed.
+'''
+
+class Solution:
+    def minSumOfProduct(self, arr1, arr2):
+        arr1.sort()
+        arr2.sort()
+        output = 0
+        for i in range(len(arr1)):
+            output += (arr1[i] * arr2[len(arr1)-i-1])
+        return output
+
+if __name__=='__main__':
+    sol = Solution()
+    print(sol.minSumOfProduct([3, 1, 1], [6, 5, 4]))
+    print(sol.minSumOfProduct([6, 1, 9, 5, 4], [3, 4, 8, 2, 4]))
