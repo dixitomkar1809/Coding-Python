90fa8125e767ede578feaeb99ace703eb45863a1
dixitomkar1809 <omedxt@gmail.com>
2020-06-23 21:29:36 -0500

    Adds subsctring

--- GFG/String/strStr.py
+++ GFG/String/strStr.py
@@ -5,6 +5,8 @@
 Your task is to implement the function strstr. The function takes two strings as arguments (s,x) and  locates the occurrence of the string x in the string s. The function returns and integer denoting the first occurrence of the string x in s (0 based indexing).
 '''
 
+# Time COmplexity: O(patternLength * stringLength)
+
 class Solution:
     def strStr(self, string, pattern):
         if len(pattern) > len(string):
