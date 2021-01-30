9fe52fd2e57f0cd4ad8496950f5a9ad03b20398e
dixitomkar1809 <omedxt@gmail.com>
2021-01-29 19:17:59 -0600

    updates

--- LeetCode/totalDurationsDivisibleBy60.py
+++ LeetCode/totalDurationsDivisibleBy60.py
@@ -1,11 +1,7 @@
 # Author: Omkar Dixit
 # Email: omedxt@gmail.com
 
-'''
-You are given a list of songs where the ith song has a duration of time[i] seconds.
-
-Return the number of pairs of songs for which their total duration in seconds is divisible by 60. Formally, we want the number of indices i, j such that i < j with (time[i] + time[j]) % 60 == 0.
-'''
+# Problem:  https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
 
 # Time Complexity: O(n)
 
